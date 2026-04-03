213982aa6f0aa7ca18ac2a8b90f3c03b273f78d1
Lélia Perez <przlelia@gmail.com>
2026-04-03 10:54:10 +0200

    feat: exercise 1 - variables and types

---
+++ 01-variables/exercice1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 22;
+    float taille = 1.60;
+    char prenom[] = "Lélia";
+    
+    printf("Jai %d ans\n", age);
+    printf("Je mesure %.2f cm\n", taille);
+    printf("Et je m'appelle %s\n", prenom);
+
+    return 0;
+}
